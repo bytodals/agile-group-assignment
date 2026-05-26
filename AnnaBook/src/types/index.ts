05a6fe1a6cc80f4a1314b644fddf5da681dfb88f
IdaEklund <ida_eklund82@hotmail.com>
2026-05-26 12:37:23 +0200

    first commit

---
+++ AnnaBook/src/types/index.ts
@@ -0,0 +1,13 @@
+
+export type BookType = {
+  _id: string;
+  title: string;
+  genre?: string;
+  author: AuthorType;
+  available: boolean;
+};
+
+export type AuthorType = {
+  _id: string;
+  name: string;
+};
