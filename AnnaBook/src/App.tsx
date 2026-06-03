cd0689695293519b3c603f8e62204d3d4ce39e3f
bytodals <todals@hotmail.com>
2026-06-03 10:34:51 +0200

    routing error fix

--- AnnaBook/src/App.tsx
+++ AnnaBook/src/App.tsx
@@ -26,7 +26,7 @@ function App() {
           />
         }
       />
-      <Route path="/my-books" element={<SavedBooksPage />} />
+      <Route path="/saved-books" element={<SavedBooksPage />} />
       <Route path="/books/:id" element={<BookDetailsPage />} />
     </Routes>
   );
